7b40f6c68d669406e7e5b361ca6182af5f569f2b
Mojtaba Yousefi <mojtaba.yousefi@live.com>
2025-10-05 18:03:42 -0400

    Remove unused import

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,6 +1,6 @@
 import uuid
 
-from sqlalchemy import Boolean, Column, ForeignKey, String
+from sqlalchemy import Boolean, ForeignKey, String
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
